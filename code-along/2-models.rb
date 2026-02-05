b46cdb54a2f98813fc461244ec18233c0c358f5e
a-v-prasad <alisha.prasad@kellogg.northwestern.edu>
2026-02-05 01:55:36 +0000

    2-models update1 -- part 2

--- code-along/2-models.rb
+++ code-along/2-models.rb
@@ -16,6 +16,15 @@ Company.destroy_all
 
 # 2. insert new rows in companies table
 
+new_company = Company.new
+p new_company
+new_company["name"] = "Apple"
+new_company ["city"] = "Cupertino"
+new_company ["state"] = "CA"
+new_company ["url"] = "https://apple.com"
+p new_company
+
+
 # 3. query companies table to find all row with California company
 
 # 4. query companies table to find single row for Apple
